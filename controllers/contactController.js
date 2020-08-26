387a9e6d3030a15ff6d923f77dbb8430e8e144d1
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-08-27 01:24:20 +0600

    update - validate name, phone number and email format

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,44 +1,62 @@
-const express = require('express');
+const express = require("express");
 var router = express.Router();
-const mongoose = require('mongoose');
-const Contact = mongoose.model('Contact');
+const mongoose = require("mongoose");
+const Contact = mongoose.model("Contact");
 
-
-router.get('/', (req, res) => {
-
-    res.render("contact/addOrEdit", {
-        viewTitle: "Add New Contact"
-    });
-
+router.get("/", (req, res) => {
+  res.render("contact/addOrEdit", {
+    viewTitle: "Add New Contact",
+  });
 });
 
-router.post('/', (req, res) => {
-
-    insertRecord(req, res);
-
+router.post("/", (req, res) => {
+  insertRecord(req, res);
 });
 
-function insertRecord(req, res){
-    var contact = new Contact();
-    contact.fullName = req.body.fullName;
-    contact.email = req.body.email;
-    contact.mobile = req.body.mobile;
-    contact.save((err, doc) => {
-
-        if(!err){
-            res.redirect('contact/list');
-
-        }else{
-            console.log('Error during record insertion : ' + err );
-        }
-
-    });
-
+function insertRecord(req, res) {
+  var contact = new Contact();
+  contact.fullName = req.body.fullName;
+  contact.email = req.body.email;
+  contact.mobile = req.body.mobile;
+  contact.save((err, doc) => {
+    if (!err) {
+      res.redirect("contact/list");
+    } else {
+      if (err.name == "ValidationError") {
+        handleValidationError(err, req.body);
+        res.render("contact/addOrEdit", {
+          viewTitle: "Insert Contact",
+          contact: req.body,
+        });
+      } else {
+        console.log("Error during record insertion : " + err);
+      }
+    }
+  });
 }
 
-router.get('/list', (req, res) => {
-    res.json('from list');
-
+router.get("/list", (req, res) => {
+  res.json("from list");
 });
+
+function handleValidationError(err, body) {
+  for (field in err.errors) {
+    switch (err.errors[field].path) {
+      case "fullName":
+        body["fullNameError"] = err.errors[field].message;
+        break;
+
+      case "mobile":
+        body["mobileError"] = err.errors[field].message;
+        break;
+      case "email":
+        body["emailError"] = err.errors[field].message;
+        break;
+
+      default:
+        break;
+    }
+  }
+}
 
 module.exports = router;
