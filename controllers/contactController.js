0411131eca5167ecc183a625a7de8c6287b713a9
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-08-26 21:50:44 +0600

    added mainLayout.hbs  and  addOrEdit.hbs ( add new contact page)

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -4,7 +4,9 @@ var router = express.Router();
 
 router.get('/', (req, res) => {
 
-    res.json('sample text');
+    res.render("contact/addOrEdit", {
+        viewTitle: "Add New Contact"
+    });
 
 });
 
--- server.js
+++ server.js
@@ -7,12 +7,24 @@ const contactController = require('./controllers/contactController');
 
 var app = express(); 
 
-app.set('views');
+
+
+app.set('views', path.join(__dirname, '/views/') );
+app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }) );
+app.set('view engine', 'hbs');
+
+
+
+
 
 app.listen(3000, () => {
 
     console.log('Express server started at port: 3000');
 
 });
+
+
+
+
 
 app.use('/contact',  contactController);
